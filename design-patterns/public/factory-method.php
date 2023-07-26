11e9797ecc14cb01541824fdaff0b31ea8a6fec2
taraszh <taras.rmrf@gmail.com>
2023-07-26 08:42:47 +0300

    add docker

--- design-patterns/public/factory-method.php
+++ design-patterns/public/factory-method.php
@@ -10,8 +10,8 @@ use App2\RandomProcessor;
 require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
 
 /*
- *  Factory Method is a creational design pattern that provides an interface for creating objects
- *  in a superclass, but allows subclasses to alter the type of objects that will be created.
+ *  Factory method pattern is a creational pattern that uses factory methods to deal with the problem of creating objects
+ *  without having to specify the exact class of the object that will be created.
  *
  *  Enables dynamic class creation
  *
